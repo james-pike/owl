3f49a75270a5e714bbf34984292c0f1e179b02f3
Tester <test@email.com>
2025-03-26 15:03:29 -0400

    commit

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -6,7 +6,7 @@ import Header from "~/components/widgets/Header";
 export default component$(() => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main class="bg-gray-900">
         <Slot />
       </main>
