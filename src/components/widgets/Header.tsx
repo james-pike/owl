2ff61b1eaaf6a2fbd461f3765c5d9a58807eb576
Tester <test@email.com>
2025-04-10 12:00:42 -0400

    commit

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -5,7 +5,6 @@ import MenuModal from "./MenuModal";
 import IconPause from "../icons/IconPause";
 import IconPlay from "../icons/IconPlay";
 import { Card } from "../ui/Card";
-import ToggleTheme from "../common/ToggleTheme";
 
 export default component$(() => {
   const store = useStore({
