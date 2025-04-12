08ee90f7fdab9748ae4dcf3befac7b01f33e8cab
Tester <test@email.com>
2025-04-12 10:21:30 -0400

    commit

--- src/components/widgets/Hero2.tsx
+++ src/components/widgets/Hero2.tsx
@@ -21,12 +21,12 @@ export default component$(() => {
               <div class="max-w-xs sm:max-w-md animate-hero-buttons m-auto flex flex-nowrap flex-col sm:flex-row sm:justify-center gap-3 lg:justify-start lg:m-0 lg:max-w-7xl">
                 <div class="flex w-full sm:w-auto">
                   <a class="btn btn-primary sm:mb-0 w-full" href="/">
-                    <IconBrandTailwind class="mr-1" /> Mint KasLords
+                    Mint KasLords
                   </a>
                 </div>
                 <div class="flex w-full sm:w-auto">
                   <a class="btn btn-secondary sm:mb-0 w-full" href="/">
-                    <IconBrandGoogle class="mr-1" /> Join The Clan
+                    Join The Clan
                   </a>
                 </div>
               </div>
