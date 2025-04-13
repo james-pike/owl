0ac79e4a496d1604e8d8f4d1cb19fad8c83732e4
Tester <test@email.com>
2025-04-12 22:38:23 -0400

    commit

--- src/components/widgets/Rarity.tsx
+++ src/components/widgets/Rarity.tsx
@@ -26,7 +26,7 @@ export default component$((props: Props) => {
   const { id, title = "", subtitle = "", highlight = "", classes = {}, isDark = false } = props;
 
   return (
-    <section class="relative scroll-mt-16" {...(id ? { id } : {})}>
+    <section class="relative scroll-mt-12" {...(id ? { id } : {})}>
         <Card.Root class="bg-gray-900">
       <div class="absolute inset-0 pointer-events-none -z-[1]" aria-hidden="true">
         <slot name="bg">
