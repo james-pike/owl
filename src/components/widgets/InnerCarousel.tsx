0e25a66a97822941af05e44c1b0ac237a3059133
Tester <test@email.com>
2025-05-15 11:03:16 -0400

    commit

--- src/components/widgets/InnerCarousel.tsx
+++ src/components/widgets/InnerCarousel.tsx
@@ -4,7 +4,7 @@ import { Card } from "../ui/Card";
 const innerRealm = [
     {
         name: "Crypto_Dutchmen",
-        role: "First Member",
+        role: "Founding Realm Member",
         // bio: "Originator of the KasLords prophecy, guardian of the Guilded Gate, and bearer of vision and vigilance.",
         image: "/images/dutchman.jpg",
         alt: "CryptoDutchman portrait",
@@ -27,7 +27,7 @@ export default component$(() => {
             id="team-grid"
             class="grid mx-auto max-w-screen-xl mt-2 md:mb-12 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4"
         >
-            {innerRealm.map(({ name, image, alt, }, index) => (
+            {innerRealm.map(({ name, role, image, alt, }, index) => (
                 <div
                     key={index}
                     class="group relative overflow-hidden rounded-lg shadow-md"
@@ -45,7 +45,7 @@ export default component$(() => {
                         </div>
                         <div class="px-4 py-3 bg-gray-100 dark:bg-gray-800">
                             <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{name}</h3>
-                            {/* <p class="text-sm text-secondary-700 dark:text-secondary-600">{role}</p> */}
+                            <p class="text-sm text-secondary-700 dark:text-secondary-600">{role}</p>
                             {/* <p class="text-sm text-gray-600 dark:text-gray-400 mb-3">{bio}</p> */}
 
                             {/* <div class="flex space-x-4">
