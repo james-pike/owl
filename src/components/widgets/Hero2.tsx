a39ebd4841376c48015da415d0b3259897668d79
your name <you@exammple.com>
2025-08-15 21:20:46 -0400

    commit

--- src/components/widgets/Hero2.tsx
+++ src/components/widgets/Hero2.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <section
-      class="md:min-h-[90vh] flex items-center bg-[url('/images/mobile2.png')] bg-cover bg-no-repeat md:bg-[url('/images/banner2.png')] bg-[center_30%]"
+      class="md:min-h-[90vh] flex items-center bg-[url('/images/mobile2.png')] bg-cover bg-no-repeat md:bg-[url('/images/banner2.png')] bg-[center_20%]"
     >
       <div class="text-center w-full px-4 md:pt-96 pt-40">
         <h2 class="text-2xl md:text-3xl text-white font-medium mb-4">KasKritterz is a fun and collectible NFT series built on the Kaspa blockchain. 250
