0cc865518e87ed2223672973691d141e2b2d1aa6
your name <you@exammple.com>
2025-08-15 20:52:47 -0400

    commit

--- src/components/widgets/Hero2.tsx
+++ src/components/widgets/Hero2.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <section
-      class="min-h-[80vh] flex items-center bg-[url('/images/mobile2.png')] bg-cover bg-center bg-no-repeat md:bg-[url('/images/logoforbanner.png')]"
+      class="min-h-[80vh] flex items-center bg-[url('/images/mobile1.png')] bg-cover bg-center bg-no-repeat md:bg-[url('/images/logoforbanner.png')]"
     >
     </section>
   );
