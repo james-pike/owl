6dba868593c0a5c84b02c12ec3d383e215d61dcb
your name <you@exammple.com>
2025-09-14 02:17:20 -0400

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -11,19 +11,18 @@ export default component$(() => {
 
         <h2 class="!text-2xl md:!text-3xl text-white font-medium mb-6 ">
           First public Auto-Mint bot on KRC-20 / KRC-721. Never miss a mint again!       </h2>
-        <a
-          href="#"
+        <div
+         
           class="bg-teal-400 text-white text-xl mx-6 px-5 py-3 rounded-lg hover:bg-teal-300 hover:text-white transition-colors flex items-center justify-center gap-2"
         >
           <LuSparkle class="w-6 h-6 group-hover:text-teal-400" /> Mint Kaspa Owls
-        </a>
+        </div>
         <div class="flex flex-col md:flex-row justify-center px-6 gap-4 md:gap-6 pt-4 pb-14">
-          <a
-            href="#"
+          <div
             class="border-2 hover:filter hover:invert border-black text-black text-xl px-5 py-3 rounded-lg bg-white/85 hover:bg-gray-100 transition-colors flex items-center justify-center gap-2">
             <LuStar class="w-6 h-6 group-hover:text-teal-400" />
             Join Telegram
-          </a>
+          </div>
 
 
 
